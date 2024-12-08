7d587f1166dced13fba7631a75e91d717c1e0647
Durga-velu <durgadevi134babu@gmail.com>
2024-12-08 14:33:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 const byteSize = (str) => {
   // write your code here
+	const obj={};
+	for(let myStr of str){
+		if(obj.hasOwnproperty(myStr)){
+			obj(myStr)=1;
+		}
+		else{
+			obj[myStr]++;
+		}
+	}
+	
 };
 
 // Do not change the code below
-const str = prompt("Enter some string.");
+const str =prompt("Enter some string.");
 alert(byteSize(str));
