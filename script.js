dd44cb352739cb3587384109efafff50c417dc6c
Durga-velu <durgadevi134babu@gmail.com>
2024-12-16 14:09:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,9 @@
 const byteSize = (str) => {
   // write your code here
-	if(str==ascii){
+	
 	return str.length;
-	}
-	else{
-		return  byteSize.value;
-	}
-};
 
-
+}
 // Do not change the code below
-const str =("Enter some string.");
-(byteSize(str));
+const str =prompt("Enter some string.");
+alert(byteSize(str));
