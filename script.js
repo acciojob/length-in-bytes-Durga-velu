602ae5c2472cccc683bc45cc7519a6fc63476681
Durga-velu <durgadevi134babu@gmail.com>
2024-12-16 13:34:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,6 @@
 const byteSize = (str) => {
   // write your code here
-	const obj={};
-	for(let myStr of str){
-		if(obj.hasOwnproperty(myStr)){
-			obj(myStr)=1;
-		}
-		else{
-			obj[myStr]++;
-		}
-	}
-	
+	return str.length;
 };
 
 // Do not change the code below
