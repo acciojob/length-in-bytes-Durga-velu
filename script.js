d8e8824e3f4da95acf2abe3d39bea55ea8c592ae
Durga-velu <durgadevi134babu@gmail.com>
2024-12-17 21:37:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const byteSize  = (str) => {
+const byteSize =(str) => {
   // write your code here
 	
 	return (str.length);
@@ -6,4 +6,4 @@ const byteSize  = (str) => {
 }
 // Do not change the code below
 const str =prompt("Enter some string.");
-alert(byteSize.(str));
+alert(byteSize(str));
